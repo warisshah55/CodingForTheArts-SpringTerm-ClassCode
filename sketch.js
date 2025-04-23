02733a5589c5ca15c116b5312dcce93513ec6281
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-04-23 14:30:23 +0100

    capital C fixed error "createCanvas"

--- sketch.js
+++ sketch.js
@@ -1,15 +1,11 @@
 function setup() {
- createCanvas (500,500);
+  createCanvas (500,500)
 }
 
 function draw() {
   background(240);
-  circle(50 ,50, 50)
+  circle(50, 50, 50)
 }
 
-let player = {
-  x:190
-  y:190
-  size: 50,
-  speed: 7
-}
+
+
