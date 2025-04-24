042850d959f0342e6925fcc65c4d3250a9633530
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-04-24 15:38:05 +0100

    changed x and y pos names and made them link throughout

--- sketch.js
+++ sketch.js
@@ -2,17 +2,33 @@
 
 //Black and White Librarian Object
 let bwLibrarian = {
-  sprite: null,
-  xPos: 150,
-  yPos: 150,
+  sprite: null, // Hold the objects sprite
+  posX: 150, // x axis position
+  posY: 150, // y axis position 
+
+  display: function() {
+    image(this.sprite, this.posX, this.posY);
+  }
   
 };
+
+// Base loading player
+function preload() {
+  bwLibrarian.sprite = loadImage("librarian-bw.png"); // Code to access the value of sprite in the bwLibrarian object
+}
 
 function setup() {
   createCanvas(500, 500); // Creates a canvas with a size of 500x500
 }
 
 function draw() {
-  
+  background(999); // White canvas
+  displayDensity(); // Handles all content to display function
 }
- 
+
+//MY FUNCTIONS
+function display() {
+  image(bwLibrarian.sprite, bwLibrarian.posX, bwLibrarian.posY) // Displays object based on data inside object 
+
+}
+
