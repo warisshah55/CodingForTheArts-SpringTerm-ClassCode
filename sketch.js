28ac4ec8e40f5107ca62925b211d763c2180ce71
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-04-24 13:59:39 +0100

    Found that starting with the "Let player" function makes it easier to define the player first to make it functional to draw.

--- sketch.js
+++ sketch.js
@@ -1,24 +1,17 @@
-// creates canvas for game
-function setup() {
-  createCanvas (500,500)
-}
-
-// sets background colour and creates circular player
-function draw() {
-  background(240);
-  circle(50, 50, 50)  
-}
-
-// creates positioning, size and speed of player when moved
+// Defines player speed, positioning and size
 let player = {
-  x: 150, y: 150, size: 50, speed: 7
+  x: 120, y: 120, size: 50, speed: 7
 };
 
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(999); // white canvas
   // Draw the player
-  circle(50, 50, 50)
-  fill(204);
+  fill(55); // Dark gray circular player
   noStroke();
-  ellipse(player.x, player.y, player.size);
+  circle(player.x, player.y, player.size,);
 
-
-
+}
