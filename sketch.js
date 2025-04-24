f107e6ad33122863fee7c2d2c6fa1a4b7d3a270e
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-04-24 13:42:38 +0100

    Let player has to go before drawing it or any functions for it to run properly

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
+// creates canvas for game
 function setup() {
   createCanvas (500,500)
 }
 
+// sets background colour and creates circular player
 function draw() {
   background(240);
-  circle(50, 50, 50)
+  circle(50, 50, 50)  
 }
 
 // creates positioning, size and speed of player when moved
 let player = {
-  x: 150,
-  y: 150,
-  size: 50,
-  speed: 7
+  x: 150, y: 150, size: 50, speed: 7
 };
 
   // Draw the player
-  fill(0, 102, 204);
+  circle(50, 50, 50)
+  fill(204);
   noStroke();
   ellipse(player.x, player.y, player.size);
 
