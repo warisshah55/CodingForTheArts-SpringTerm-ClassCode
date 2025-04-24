4b8ec2ca81e13b11a6377ba7642b9c7470345501
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-04-24 13:21:56 +0100

    creates positioning, speed and size of the player, however does not apply to the circle so i may need to change order of code to make the circle a player.

--- sketch.js
+++ sketch.js
@@ -7,5 +7,18 @@ function draw() {
   circle(50, 50, 50)
 }
 
+// creates positioning, size and speed of player when moved
+let player = {
+  x: 150,
+  y: 150,
+  size: 50,
+  speed: 7
+};
+
+  // Draw the player
+  fill(0, 102, 204);
+  noStroke();
+  ellipse(player.x, player.y, player.size);
+
 
 
