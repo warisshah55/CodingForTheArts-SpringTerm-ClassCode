19ed26fbb02c561ebce8bfc791432736343655b3
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-03 15:36:08 +0100

    character better defines than players

---
+++ characters.js
@@ -0,0 +1,27 @@
+//INITIAL VARIABLES
+
+//Data for both characters
+let bwLibrarian; // contains object
+let bwLibrarianSprite; // contains sprite 
+let bwLibrarianXpos = 50; //position on X axis 
+let bwLibrarianYpos = 100; // position on Y axis
+let bwLibrarianHitPoints = 10; // variable for hitpoints 
+
+let pinkLibrarian // contains object
+let pinkLibrarianSprite // contains sprite
+let pinkLibrarianXpos = 200; // position on X axis
+let pinkLibrarianYpos = 100; // position on Y axis
+let pinkLibrarianHitPoints = 5; // variable for hitpoints 
+
+let textPaddingX = 50; // same applies to both characters
+let textPaddingY = -25; // same applies to both characters
+
+//p5 FUNCTIONS
+
+function setup () {
+    createCanvas(400, 400);  
+}
+
+function draw() {
+    background(220);
+}
