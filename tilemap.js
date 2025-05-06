70a547ef2fc1a7f0eafd8dbbfab98f28cb7d9fa1
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-06 12:40:29 +0100

    added tile as a class

---
+++ tilemap.js
@@ -0,0 +1,31 @@
+
+//Data for the tiles
+let tileMap = []; //This is an array that will store the tiles in later
+let tilesX = 10; //How many tiles there will be on the x axis (horizontally)
+let tilesY = 10; //How many tiles there will be on the y axis (vertically)
+let tileSize = 50; //How many pixels across each tile will be.
+
+
+
+
+
+function setup() {
+    createCanvas(500,500)
+}
+
+
+function draw() {
+    background(0)
+}
+
+
+//CLASSES
+class Tile {
+    constructor(tileX, tileY, tileSize, tileID) {
+        this.tileX = tileX;
+        this.tileY = tileY;
+        this.tileSize = tileSize;
+        this.tileID = tileID;
+    }
+    
+}
