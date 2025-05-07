34ac7b13dfabd5044a0fb7f30b040fd9de3d2385
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-07 14:29:43 +0100

    array tiles created without errors

--- tilemap.js
+++ tilemap.js
@@ -6,6 +6,20 @@ let tilesX = 10; //How many tiles there will be on the x axis (horizontally)
 let tilesY = 10; //How many tiles there will be on the y axis (vertically)
 let tileSize = 50; //How many pixels across each tile will be.
 
+let graphicsMap = [
+//              Y VALUES
+//   0  1  2  3  4  5  6  7  8  9
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 0
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 1
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0], // 2
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 3
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 4    X VALUES
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 5
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 6
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 7
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], // 8
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  // 9
+]
 
    
 
