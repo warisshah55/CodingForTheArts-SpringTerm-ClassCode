d70affb461099a80779debdc08f39e9c6e807978
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-08 13:11:18 +0100

    CREATED PLAYER CLASS

--- sketch.js
+++ sketch.js
@@ -81,3 +81,36 @@ class Character {
     this.hitPoints -= damage; //Takes away the value of damage from the character's hitPoints
    }
 }
+
+
+class Player{
+  constructor(sprite, startX, startY, tileSize, tileRules) {
+    //PLAYER SPRITES
+    this.sprite = sprite;
+
+    //TILE POSITION DATA
+    this.tileX = startX,
+    this.tileY = startY,
+
+    //PIXEL POSITION DATA
+    this.xPos = startX * tileSize;
+    this.yPos = startY * tileSize;
+
+    //DIRECTION PLAYER WANTS TO MOVE
+    this.dirX = 0;
+    this.dirY = 0;
+
+    //PLAYER'S TARGET PIXEL POSITION
+    this.tx = this.xPos;
+    this.ty = this.yPos;
+
+    //MOVEMENT
+    this.isMoving = false;
+    this.speed = 5;
+
+    // TILE DATA
+    this.tileSize = tileSize;
+    this. tileRules = tileRules;
+
+  }
+}
