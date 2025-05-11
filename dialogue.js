f0a333f7e3d107906e7a807c0be7b705620c4c6e
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-11 17:11:00 +0100

    1. ADJUSTED ALL TEXT SIZE, COLOUR ETC 2. ADDED PRESS === TO CONTINUE IN ALL dialogue 3. ADDED ALL CODE COMMENTS 4. NOTE, I started with getting my "let" variables together and implemented it in to my tilemap.js first before moving on to dialogue.js as it was easier for me to deal with the small inserts then doing the whole dialogue.js.

--- dialogue.js
+++ dialogue.js
@@ -1,42 +1,43 @@
-// dialogue.js
-
-let dialogueActive = false;
+//INITIALISE DIALOGUE VARIABLES
+let dialogueActive = false; 
 let dialogueTriggered = false;
 let currentDialogueIndex = 0;
 
-let dialogueLines = [
-    "Slimeball: WHO DARES to step on my sacred tile!?",
-    "HeatBlast: Just passing through you Goo Ball",
-    "Slimeball: That's not my name, have fun getting past me!",
-    "CHALLENGE- **PASS SLIMEBALL AND FIND THE SECRET EXIT**"
+let dialogueLines = [ //Array holding every line of dialogue in sequence
+    "Slimeball: WHO DARES to step on my sacred tile!?  press SPACE to continue...", //Dialogue 1
+    "HeatBlast: Just passing through you Goo Ball!!  *press SPACEBAR*", //Dialogue 2
+    "Slimeball: That's not my name, have fun getting past me!  *press SPACEBAR*", //Dialogue 3
+    "CHALLENGE- **PASS SLIMEBALL AND FIND THE SECRET EXIT**" //Dialogue 4 (Alert message)
 ];
 
 function startDialogue() {
     dialogueActive = true;
-    dialogueTriggered = true;
-    currentDialogueIndex = 0;
+    dialogueTriggered = true; //Makes sure dialogue doesn't trigger again
+    currentDialogueIndex = 0; //Makes sure dialogue happens in order of sequence
 }
 
 function drawDialogueBox() {
     if (dialogueActive) {
-        fill(0, 0, 0, 200);
-        rect(0, height - 100, width, 100);
+        fill(0); //Black text box for dialogue
+        rect(0, height - 100, width); //Position of dialogue box (bottom of screen)
 
-        fill(255);
-        textSize(16);
-        textAlign(LEFT, TOP);
+        fill(139, 0, 0); //Maroon red text
+        textSize(13); //Size of text
+        textAlign(LEFT, TOP); //Position of dialogue
         text(dialogueLines[currentDialogueIndex], 20, height - 30);
     }
 }
 
+
 function keyPressed() {
-    if (dialogueActive &&
-        key === ' ') {
-            currentDialogueIndex++;
+    if (dialogueActive && //Only run function when dialogue is showing
+        key === ' ') { //& if spacebar is being pressed
+            currentDialogueIndex++; //Goes to next line of dialogue
             if (currentDialogueIndex >= dialogueLines.length) {
-                dialogueActive = false;
-            }
+                dialogueActive = false; //If dialogue is done remove dialogue box
+
         }
+    }
 }
 
 
