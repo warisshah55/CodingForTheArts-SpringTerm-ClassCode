696df39c7811db5152b490142f7c265168c5187c
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-11 16:41:31 +0100

    added startdialogue function

--- dialogue.js
+++ dialogue.js
@@ -1,14 +1,19 @@
-//INITIALISE DIALOGUE VARIABLES
+// dialogue.js
 
 let dialogueActive = false;
 let dialogueTriggered = false;
-let dialogueCurrentIndex = 0;
+let currentDialogueIndex = 0;
 
 let dialogueLines = [
     "Slimeball: WHO DARES to step on my sacred tile!?",
     "HeatBlast: Just passing through you Goo Ball",
     "Slimeball: That's not my name, have fun getting past me!",
     "CHALLENGE- **PASS SLIMEBALL AND FIND THE SECRET EXIT**"
-
 ];
+
+function startDialogue() {
+    dialogueActive = true;
+    dialogueTriggered = true;
+    currentDialogueIndex = 0;
+}
 
