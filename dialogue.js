430b799bf62f60fe0194d5a1f65bb3bdc93ba99e
warisshah55 <warisrazashah5@hotmail.co.uk>
2025-05-11 16:31:00 +0100

    ADDED DIALOGUE LINES AND OBJECTIVE

--- dialogue.js
+++ dialogue.js
@@ -5,6 +5,10 @@ let dialogueTriggered = false;
 let dialogueCurrentIndex = 0;
 
 let dialogueLines = [
+    "Slimeball: WHO DARES to step on my sacred tile!?",
+    "HeatBlast: Just passing through you Goo Ball",
+    "Slimeball: That's not my name, have fun getting past me!",
+    "CHALLENGE- **PASS SLIMEBALL AND FIND THE SECRET EXIT**"
 
-    
-]
+];
+
